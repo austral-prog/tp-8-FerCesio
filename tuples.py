c8f9704d67d1c4d0274e04ebe475babc4c8a0c7e
FerCesio <162721896+FerCesio@users.noreply.github.com>
2024-05-24 11:52:37 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,19 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
+
+def get_coordinate(x):
+    return x[1]
+get_coordinate(("hola", "2A"))
 
 
-def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+def convert_coordinate(x):
+    return (x[0],x[1])
+convert_coordinate("2A")
 
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
-
-def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+def create_record(x,y):
+    a,b = y[1]
+    if x[1] ==  (a+b):
+        return x+y
+    else:
+        return "not a match"
+create_record(('Brass Spyglass', '4B'), ('Abandoned Lighthouse', ('4', 'B'), 'Blue'))
 
 
-def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
